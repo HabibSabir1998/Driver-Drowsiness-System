63045d41175b47953f001416ffe52e76a93e6594
Habib1998 <Habib.kasbati@gmail.com>
2020-02-08 00:25:37 +0500

    Login Form ready

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import UserLogin from "./Components/UserLogin";
+import UserSignup from "./Components/UserSignup";
+
 
 function App() {
   return (
+  <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Driver Drowsiness System<span>For Safety Journey</span>
-        </h1>
-        
-      </header>
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/sign-in"}>Driver Drowsiness System</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={UserLogin} />
+            <Route path="/sign-in" component={UserLogin} />
+            <Route path="/sign-up" component={UserSignup} />
+          </Switch>
+        </div>
+      </div>
     </div>
+    </Router>
   );
 }
 
